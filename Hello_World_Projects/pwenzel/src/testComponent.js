7b1e69fb4d9f197300224622e706873d390f5ef7
Patrick Wenzel <pwenzel@iastate.edu>
2020-10-19 16:04:59 -0500

    Making Hello World App

---
+++ Hello_World_Projects/pwenzel/src/testComponent.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export function TestComponent(){
+    return (
+        <>
+            <p>Welcome to Patrick Wenzel's "Hello World" App</p>
+            <a
+                className="App-link"
+                href="http://sdmay21-23.sd.ece.iastate.edu/"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+                Visit Our Team's Website
+            </a>
+        </>
+    )
+}
