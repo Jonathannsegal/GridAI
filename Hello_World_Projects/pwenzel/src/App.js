6dffebae4d9148f943a07729fcc6324c1cdf5db1
Patrick Wenzel <pwenzel@iastate.edu>
2020-10-19 16:11:50 -0500

    test

--- Hello_World_Projects/pwenzel/src/App.js
+++ Hello_World_Projects/pwenzel/src/App.js
@@ -9,7 +9,6 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <TestComponent />
-        
       </header>
     </div>
   );
