8557d8fc2c02745753f14fb33d8e024e29561c37
Karthik Prakash <kprakash@iastate.edu>
2020-10-21 10:10:24 -0500

    implemented /time endpoint

--- Hello_World_Projects/pwenzel/src/App.js
+++ Hello_World_Projects/pwenzel/src/App.js
@@ -1,14 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { TestComponent } from './testComponent'
 
 function App() {
+  var [currentTime, setCurrentTime] = useState("N/A");
+
+  useEffect(() => {
+    const interval = setInterval(() => {fetch('/time').then(res => res.json()).then(data => {
+      setCurrentTime(data.time);
+    })
+  }, 1000);
+  return () => clearInterval(interval);
+}, []);
+  
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <TestComponent />
+        <p> The date is {currentTime}.</p>
       </header>
     </div>
   );
