0c3be0cddfce68db1caec9fa3b8a8179ead6f2ec
patrickjwenzel <pwenzel@iastate.edu>
2021-02-25 21:32:05 -0600

    Dockerized npm, trying to set up Flask

--- material-dashboard-react-master/src/index.js
+++ material-dashboard-react-master/src/index.js
@@ -21,10 +21,10 @@ import { createBrowserHistory } from "history";
 import { Router, Route, Switch, Redirect } from "react-router-dom";
 
 // core components
-import Admin from "./layouts/Admin.js";
-import RTL from "./layouts/RTL.js";
+import Admin from "layouts/Admin.js";
+import RTL from "layouts/RTL.js";
 
-import "./assets/css/material-dashboard-react.css";
+import "assets/css/material-dashboard-react.css";
 
 const hist = createBrowserHistory();
 
