613e8d3e4b613ce79c105036ab625bf179e287d0
abir99 <abir99@iastate.edu>
2021-04-01 00:32:33 -0500

    Working Chart and updated Graphdisplay.js

--- GridAI-Dashboard/client/src/components/GraphDisplay/graphdisplay.js
+++ GridAI-Dashboard/client/src/components/GraphDisplay/graphdisplay.js
@@ -1,12 +1,26 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import { Graph } from "react-d3-graph"
-
-
+import StickyHeadTable from "../Table/StickyHeadTable.js"
+import LineChart from "../Charts/linechart.js"
 
   // the graph configuration, just override the ones you need
+
+  //var lineData = [['x','kWh'],['T_2040',4.56]];
+
+  const graphstyle = {
+    height: "600px",
+    width: "1000px"
+  }
+
+  const chartstyle = {
+    position: "absolute",
+    top: "80px",
+    right: "0"
+  }
+
   const myConfig = {
-    height: 800,
-    width: 750,
+    height: 600,
+    width: 1000,
     staticGraph: true,
     staticGraphWithDragAndDrop: false,
     nodeHighlightBehavior: true,
@@ -21,66 +35,65 @@ import { Graph } from "react-d3-graph"
     },
   };
 
-  const onClickNode = function(nodeId) {
-    window.alert(`Clicked node ${nodeId}`);
-  };
-
   const onClickLink = function(source, target) {
     window.alert(`Clicked link between ${source} and ${target}`);
   };
-
-  //const topContainer = useRef();
 
 class GraphDisplay extends Component{
 
   constructor(props) {
     super(props);
     this.state = {
-      coords:[],
+      lineData:[['x','kWh']],
+      nodeData:[],
       isLoaded: false,
     }
+
+    this.onClickNode = this.onClickNode.bind(this)
   }
-
-  // componentDidMount() {
-
-  //   Promise.all([
-
-  //   fetch('/coords'),
-  //   fetch('/lineA')])
-  //   .then(([res1, res2]) => Promise.all([res1.json(),res2.json()]))
-  //   .then(([data1, data2]) => this.setState({
-
-  //     coords:data1, lineA:data2, isLoaded:true
-  //   }))
-
-    
-  // }
 
   componentDidMount() {
-    this.fetchCoords();
+    this.fetchNodeData();
   }
 
-  fetchCoords = () => {
-    fetch('/coordinates').then(res1 => res1.json()).then((data1) => this.setState({
-    coords:data1,
-    isLoaded:true
-    }));
+  fetchNodeData = () =>{
+    fetch('/alldata').then(res1 => res1.json()).then((data1) => this.setState({
+      nodeData:data1,
+      isLoaded:true
+      }));
   }
-  
+
+  fetchValue = async(node) => {
+    const resp = await fetch(`/CurrentValues/${node}`);
+    const data = await resp.json();
+    return data;
+  }
+
+  onClickNode = async(nodeID)=>{
+    const val = await this.fetchValue(nodeID);
+    console.log(val[0]);
+    window.alert(`Current Value: ${val[0].currentval}`);
+    let tempdata = [nodeID,val[0].currentval]
+    let temparr = this.state.lineData;
+    temparr.push((tempdata))
+    console.log("this is tempArr in function")
+    console.log(temparr)
+    this.setState({lineData:temparr})
+  };
 
   render(){
-
-
+    
+    console.log(this.state.nodeData)
     if(!this.state.isLoaded){
       return <div>Loading...</div>;
     }
 
     else{
 
-      var {coords} = this.state;
+      var {nodeData,lineData} = this.state;
       let data = [];
-      for(let i=0;i<coords.length;i++){
-        data.push({id: coords[i].BusID, x:coords[i].X*50+100, y:coords[i].Y*-50+750})
+      for(let i=0;i<nodeData[0].length;i++){
+        data.push({id: nodeData[0][i].BusID, x:nodeData[0][i].X*50+100, y:nodeData[0][i].Y*-50+750})
       }
 
       // let link = [];
@@ -95,16 +108,27 @@ class GraphDisplay extends Component{
       // }
 
       return (
-        <div className="App">
+      <div>
+        <div style={chartstyle}>
+          {
+          console.log("this is lineData in render"),
+          console.log(lineData),
+          <LineChart data={lineData}/>
+        }
+        </div>
+        <div style={graphstyle}>
         {
-          <Graph 
-      id="graph-id" // id is mandatory
-      data={{nodes:data}}
-      config={myConfig}
-      onClickNode={onClickNode}
-      onClickLink={onClickLink}
-      /> }
-      </div>
+          <Graph
+          id="graph-id" // id is mandatory
+          data={{nodes:data,links:[]}}
+          config={myConfig}
+          onClickNode={this.onClickNode}
+          onClickLink={onClickLink}
+          /> 
+        }
+        </div>
+
+    </div>
       );
     } 
 
