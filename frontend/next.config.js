4fa2fab3296b65ef88064600416120aa3869804f
jsegal <jsegal@iastate.edu>
2022-01-23 08:38:29 +0000

    CI_CD #32

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -2,4 +2,4 @@ module.exports = {
   experimental: {
     outputStandalone: true,
   },
-}
+};
--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,7 +1,9 @@
-import '../styles/globals.css'
+/* eslint react/prop-types: 0 */
+
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
