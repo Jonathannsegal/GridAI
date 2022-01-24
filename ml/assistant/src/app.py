f3ccd7eaf504eb4a65913a68e52fb8a431492587
jsegal <jsegal@iastate.edu>
2022-01-24 00:45:09 +0000

    clean up structure and add all services #51

---
+++ ml/assistant/src/app.py
@@ -0,0 +1,22 @@
+"""Imports"""
+import os
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+def func(value):
+    """Add 2 to value"""
+    return value + 2
+
+
+@app.route("/")
+def hello_world():
+    """Hello World Endpoint"""
+    name = os.environ.get("NAME", "World")
+    return f"Grid AI: assistant, Hello {name}!"
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
---
+++ ml/assistant/tests/test_assistant.py
@@ -0,0 +1,4 @@
+from assistant.src.app import *
+
+def test_answer():
+    assert func(3) == 5
