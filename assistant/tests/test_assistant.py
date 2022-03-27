c69dc12a6f11ad4f7ae5d4a5adb253d171c4e59a
har1 <har1@iastate.edu>
2022-03-27 21:26:02 +0000

    59

--- assistant/tests/test_assistant.py
+++ assistant/tests/test_assistant.py
@@ -1,4 +1,14 @@
+from pathlib import Path
+from unittest import mock
 from assistant.src.app import *
+from assistant.src.speech_to_text import *
+
+
+def speech_test():
+    """speech to text test"""
+    mock.patch.dict(os.environ, {"GOOGLE_APPLICATION_CREDENTIALS": Path.cwd() / "assistant/client_secret/speechtotext-331119-820cdd23000d.json"})
+    assert speech_text() == "how old is the Brooklyn Bridge"
 
 def test_text_request():
     """Tests a text request sent to the Assistant"""
+
