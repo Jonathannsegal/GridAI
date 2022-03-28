4c6bbb99a40f9be318c53d91121d8dece76defa5
har1 <har1@iastate.edu>
2022-03-28 15:35:56 +0000

    78

--- assistant/src/speech_to_text.py
+++ assistant/src/speech_to_text.py
@@ -1,40 +1,35 @@
 """Method for speech-to-text"""
-# import io
+import io
 # import click
-# from google.cloud import speech
+from google.cloud import speech
 
 
-# @click.command()
-# @click.option('--gcs-audio', '-i', required=True,
-#               metavar='<input file>', type=str,
-#               help='Path to input audio file (format: LINEAR16 16000 Hz).')
-def speech_text():
-    """Convert provided audio file into a text transcript"""
+def speech_text(input_audio_file, gcs):
+    """Convert provided audio file into a text transcript
+    if gcs = true use google cloud file, if gcs = false use local file"""
 
     # Instantiates a client
-    # client = speech.SpeechClient()
+    client = speech.SpeechClient()
 
-    # with io.open(input_audio_file, "rb") as audio_file:
-    #     content = audio_file.read()
+    if gcs:
+        audio = speech.RecognitionAudio(uri=input_audio_file)
+    else:
+        with io.open(input_audio_file, "rb") as audio_file:
+            content = audio_file.read()
+        audio = speech.RecognitionAudio(content=content)
 
-    # audio = speech.RecognitionAudio(uri=gcs_audio)
-    # audio = speech.RecognitionAudio(content=content)
-    # config = speech.RecognitionConfig(
-    #     encoding=speech.RecognitionConfig.AudioEncoding.LINEAR16,
-    #     sample_rate_hertz=16000,
-    #     language_code="en-US",
-    # )
+    config = speech.RecognitionConfig(
+        encoding=speech.RecognitionConfig.AudioEncoding.LINEAR16,
+        sample_rate_hertz=16000,
+        language_code="en-US",
+    )
 
     # Detects speech in the audio file
-    # response = client.recognize(config=config, audio=audio)
+    response = client.recognize(config=config, audio=audio)
 
-    # return_str = ""
+    return_str = ""
 
-    # for result in response.results:
-    #     return_str += result.alternatives[0].transcript
-    # print(f"Transcript: {return_str}")
-    return "how old is the Brooklyn Bridge"
-
-
-# if __name__ == '__main__':
-#     speech_text()  # pylint: disable=no-value-for-parameter
+    for result in response.results:
+        return_str += result.alternatives[0].transcript
+    print(f"Transcript: {return_str}")
+    return return_str
