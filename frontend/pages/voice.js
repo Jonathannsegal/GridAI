3ad37a519766467b80371d00a50ffb6722fdba6d
jsegal <jsegal@iastate.edu>
2022-04-05 22:45:41 +0000

    Assistantfrontend

--- frontend/pages/voice.js
+++ frontend/pages/voice.js
@@ -1,40 +1,10 @@
-/* eslint-disable no-unused-vars */
-/* eslint-disable import/no-extraneous-dependencies */
-/* eslint-disable import/extensions */
-/* eslint-disable react/prop-types */
-import * as React from 'react';
-// import Launcher from 'react-chat-window';
-import { useEffect } from 'react';
-import { Widget, addResponseMessage } from 'react-chat-widget';
-import ChatWidget from '../components/ChatWidget';
-import Metatags from '../components/Metatags';
-import { sendTextRequest } from '../lib/calls';
+import dynamic from 'next/dynamic';
 
-export default function Voice() {
-  // useEffect(() => {
-  //   addResponseMessage('Welcome to this awesome chat!');
-  // }, []);
+const DynamicComponentWithNoSSR = dynamic(() => import('../components/voice'), {
+  ssr: false,
+});
 
-  const handleNewUserMessage = async (newMessage) => {
-    const responce = await sendTextRequest(newMessage);
-    // eslint-disable-next-line no-console
-    console.log(`New message incoming!  ${responce}`);
-    // Now send the message throught the backend API
-    addResponseMessage(`${responce}`);
-  };
-
-  return (
-    <main>
-      <Metatags title="Grid AI" description="GridAI" />
-
-      <p>welcome to voice</p>
-
-      <Widget
-        handleNewUserMessage={handleNewUserMessage}
-        title="GridAI"
-        subtitle=""
-      />
-
-    </main>
-  );
+// eslint-disable-next-line func-names
+export default function () {
+  return <DynamicComponentWithNoSSR />;
 }
--- frontend/pages/voices.js
+++
@@ -1,9 +0,0 @@
-import dynamic from 'next/dynamic';
-
-const DynamicComponentWithNoSSR = dynamic(() => import('./voice'), {
-  ssr: false,
-});
-
-export default function () {
-  return <DynamicComponentWithNoSSR />;
-}
