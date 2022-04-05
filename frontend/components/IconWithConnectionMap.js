3ba4b38a43a77ec2648fecb27648481e472fa435
Marissa Gustafson <marissag@iastate.edu>
2022-04-05 18:14:05 -0500

    Icons and connection on same map yayyy

---
+++ frontend/components/IconWithConnectionMap.js
@@ -0,0 +1,79 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable react/prop-types */
+/* eslint-disable max-len */
+/* eslint-disable no-console */
+/* eslint-disable import/no-extraneous-dependencies */
+import DeckGL from '@deck.gl/react';
+import { useState } from 'react';
+import MapGL from 'react-map-gl';
+import {
+  getConnections, getLineLayer, getIconLayer,
+} from '../lib/calls';
+
+function IconWithConnectionMap() {
+  const MAPBOX_TOKEN = 'pk.eyJ1IjoibWFyaXNzYWciLCJhIjoiY2t6aXZ5M2FkNGZiNTJ3bmZ1Ymx4cXEzaSJ9.oxEaAW-mjM0Cc9NDNfDQPg'; // Set your mapbox token here
+
+  // Viewport settings
+  const INITIAL_VIEW_STATE = {
+    longitude: -112.337088,
+    latitude: 33.75363564,
+    zoom: 12,
+    pitch: 0,
+    bearing: 0,
+  };
+  const [viewport, setViewport] = useState({
+    latitude: 42.0281,
+    longitude: -93.6422,
+    zoom: 12,
+    bearing: 0,
+    pitch: 0,
+  });
+
+  const data = getConnections();
+
+  //const data = getConnections();
+  /**
+   * Data format:
+   * [
+   *   {
+   *     inbound: 72633,
+   *     outbound: 74735,
+   *     from: {
+   *       name: '19th St. Oakland (19TH)',
+   *       coordinates: [-122.269029, 37.80787]
+   *     },
+   *     to: {
+   *       name: '12th St. Oakland City Center (12TH)',
+   *       coordinates: [-122.271604, 37.803664]
+   *   },
+   *   ...
+   * ]
+   */
+  const lineLayer = getLineLayer();
+
+  const iconLayer = getIconLayer();
+
+  const layers = [
+    iconLayer, lineLayer,
+  ];
+
+  return (
+    <DeckGL
+      initialViewState={INITIAL_VIEW_STATE}
+      controller
+      layers={layers}
+    >
+      <MapGL
+        className="rounded-lg"
+        {...viewport}
+        width="100%"
+        height="30em"
+        mapStyle="mapbox://styles/mapbox/light-v10"
+        onViewportChange={setViewport}
+        mapboxApiAccessToken={MAPBOX_TOKEN}
+      />
+    </DeckGL>
+  );
+}
+
+export default IconWithConnectionMap;
--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable linebreak-style */
 /* eslint-disable no-unused-vars */
 /* eslint-disable import/no-extraneous-dependencies */
 /* eslint-disable import/extensions */
@@ -5,7 +6,7 @@
 import * as React from 'react';
 import Metatags from '../components/Metatags';
 import Button from '../components/Button';
-import HoverMap from '../components/HoverMap';
+import IconWithConnectionMap from '../components/IconWithConnectionMap';
 import {
   getCurrentVoltage,
   getCoordinates,
@@ -26,8 +27,8 @@ export default function Home() {
             <div className="relative w-full bg-cover lg:w-6/12 xl:w-8/12 ">
               <div className="relative flex flex-col items-center justify-center w-full h-full px-0 my-0 lg:px-10 lg:my-0">
                 <div className="container px-8 mx-auto sm:px-0 xl:px-0">
-                  <div className="w-full px-0 py-0 mx-auto mt-5 bg-white border border-gray-200 rounded-lg sm:px-0 md:px-0 sm:py-0 sm:shadow">
-                    <HoverMap />
+                  <div id="Map" className="w-full px-0 py-0 mx-auto mt-5 bg-white border border-gray-200 rounded-lg sm:px-0 md:px-0 sm:py-0 sm:shadow">
+                    <IconWithConnectionMap />
                   </div>
                 </div>
               </div>
