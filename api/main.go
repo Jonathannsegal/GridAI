d72ab876bd493586a8003f678650bf84c9ffd700
Marissa Gustafson <marissag@iastate.edu>
2022-04-22 15:00:22 -0500

    api no cors for getAllCoordinates

--- api/main.go
+++ api/main.go
@@ -338,7 +338,7 @@ func getAllCoordinates(w http.ResponseWriter, r *http.Request) {
 		}
 		Nodes = append(Nodes, node)
 	}
-	w.Header().Set("Access-Control-Allow-Origin", os.Getenv("ACAO"))
+	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	jsonResponse, err := json.Marshal(Nodes)
